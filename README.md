10c8269cd6308c68fbe2c6ad98298ac5957abd2b
xuyue <tjxuyue@163.com>
2018-07-05 00:36:26 +0800

    基本功能

--- README.md
+++ README.md
@@ -1,2 +1,12 @@
 # Hyperledger Fabric 0.6 区块链浏览器 接口服务
 
+
+##获取当前区块高度和区块信息
+/getBlockNum 
+##获取节点列表
+/getPeerList
+##获取指定区块信息
+/getBlock/{num}
+##获取指定交易信息
+/getTransaction/{txid}
+
--- src/main/java/com/project/controller/DemoController.java
+++ src/main/java/com/project/controller/DemoController.java
@@ -1,9 +1,13 @@
 package com.project.controller;
 
 import java.util.HashMap;
+import java.util.List;
+
+import javax.websocket.server.PathParam;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Value;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -33,23 +37,19 @@ public class DemoController {
 	static private String URL_PRE_HTTP = "http://";
 	static private String URL_PRE_HTTPS = "https://";
 	static private String url1 = "/chain";
+	static private String url2 = "/network/peers";
+	static private String url3 = "/chain/blocks";
+	static private String url4 = "/transactions";
 
-	@RequestMapping(value = "test")
-	public String test() {
-		Demo demo = new Demo();
-		demo.setId(1);
-		demo.setName("test");
-		demoService.demo(demo);
-		return "测试成功";
-	}
-
-	@RequestMapping(value = "chain")
-	public String chain() {
-		HashMap<String, Object> map = new HashMap<String, Object>();
-		map.put("height", 231);
-		return JSON.toJSONString(map);
-	}
-
+	/*
+	 * @RequestMapping(value = "test") public String test() { Demo demo = new
+	 * Demo(); demo.setId(1); demo.setName("test"); demoService.demo(demo); return
+	 * "测试成功"; }
+	 * 
+	 * @RequestMapping(value = "chain") public String chain() { HashMap<String,
+	 * Object> map = new HashMap<String, Object>(); map.put("height", 231); return
+	 * JSON.toJSONString(map); }
+	 */
 	@RequestMapping(value = "getBlockNum")
 	public ResponseBody getBlockNum() {
 
@@ -65,11 +65,85 @@ public class DemoController {
 		String result = restfulService.get(url.toString(), new HashMap<String, Object>());
 		JSONObject json = JSON.parseObject(result);
 		if (json.containsKey("height")) {
-			responseBody.setData(json.get("height").toString());
+			responseBody.setData(json.toString());
 			responseBody.setStatus(SUCCESS);
 			return responseBody;
 		}
 		responseBody.setStatus(ERROR);
 		return responseBody;
 	}
+
+	@RequestMapping(value = "getPeerList")
+	public ResponseBody getPeerList() {
+
+		ResponseBody responseBody = new ResponseBody();
+
+		StringBuffer url = new StringBuffer();
+		url.append(URL_PRE_HTTP);
+		url.append(ip2);
+		url.append(":");
+		url.append(port2);
+		url.append(url2);
+		System.out.println(url);
+		String result = restfulService.get(url.toString(), new HashMap<String, Object>());
+		JSONObject json = JSON.parseObject(result);
+		if (json.containsKey("peers")) {
+			responseBody.setData(json.getString("peers"));
+			responseBody.setStatus(SUCCESS);
+			return responseBody;
+		}
+		responseBody.setStatus(ERROR);
+		return responseBody;
+	}
+
+	@RequestMapping(value = "getBlock/{num}")
+	public ResponseBody getBlockInfor(@PathVariable("num") String num) {
+
+		ResponseBody responseBody = new ResponseBody();
+
+		StringBuffer url = new StringBuffer();
+		url.append(URL_PRE_HTTP);
+		url.append(ip2);
+		url.append(":");
+		url.append(port2);
+		url.append(url3);
+		url.append("/");
+		url.append(num);
+		System.out.println(url);
+		String result = restfulService.get(url.toString(), new HashMap<String, Object>());
+		JSONObject json = JSON.parseObject(result);
+		if (!json.containsKey("Error")) {
+			responseBody.setData(json.toJSONString());
+			responseBody.setStatus(SUCCESS);
+			return responseBody;
+		}
+		responseBody.setStatus(ERROR);
+		return responseBody;
+	}
+
+	@RequestMapping(value = "getTransaction/{txid}")
+	public ResponseBody getTransaction(@PathVariable("txid") String txid) {
+
+		ResponseBody responseBody = new ResponseBody();
+
+		StringBuffer url = new StringBuffer();
+		url.append(URL_PRE_HTTP);
+		url.append(ip2);
+		url.append(":");
+		url.append(port2);
+		url.append(url4);
+		url.append("/");
+		url.append(txid);
+		System.out.println(url);
+		String result = restfulService.get(url.toString(), new HashMap<String, Object>());
+		JSONObject json = JSON.parseObject(result);
+		if (!json.containsKey("Error")) {
+			responseBody.setData(json.toJSONString());
+			responseBody.setStatus(SUCCESS);
+			return responseBody;
+		}
+		responseBody.setStatus(ERROR);
+		return responseBody;
+	}
+
 }
